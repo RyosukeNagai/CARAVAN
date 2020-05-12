292610711cf6b139c7ad47ede8d1d2bc1c5a7c7a
長井崚介 <ryooo@nagainoMacBook-Pro.local>
2020-05-12 11:35:28 +0900

    [Continued] 1

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -18,20 +18,24 @@ end
       if book.save
         flash[:id] = 'Book was successfully created.'
   	    redirect_to book_path(book.id)
-      
+      # else render :new
+
     end
-      
   end
 
   def edit
     @book = Book.find(params[:id])
+    
   end
 
   def update
       book = Book.find(params[:id])
-      book.update(book_params)
+      if book.update(book_params)
       flash[:id] = 'Book was successfully updated.'
       redirect_to book_path(book.id)
+      # elserender :edit
+
+    end
   end
 
   def destroy
--- Bookers/app/models/book.rb
+++ Bookers/app/models/book.rb
@@ -1,4 +1,6 @@
-class Book < ActiveRecord::Base
-validates :title, presence: true
-validates :body, presence: true
+class Book < ApplicationRecord
+
+
+validates :title, {presence: true}
+validates :body, {presence: true}
 end
